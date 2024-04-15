2d0e3423c18a477391afdfc6d650ebe753979cdc
Abdul Gaffar <csabdulgaffar@gmail.com>
2024-04-15 17:02:50 +0530

    'aboutupdated'

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,9 @@
 
 function About() {
     return (
-        <>
+        <div className="min-h-screen p-6">
             <div className="text-center text-3xl">Developed by csabdulgaffar@gmail.com</div>
-        </>
+        </div>
 
     )
 }
